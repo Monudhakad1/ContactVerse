cc37d9de1dbe24fe7f377d37bdc0d2f666b63782
Monudhakad1 <monudhakad055@gmail.com>
2025-06-17 23:47:52 +0530

    done for today

--- src/main/java/com/contactverse/contactverse/controllers/PageController.java
+++ src/main/java/com/contactverse/contactverse/controllers/PageController.java
@@ -6,11 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class pageController {
+public class PageController {
 
     @RequestMapping("/home")
     public String home(Model model, ServletRequest servletRequest){
-        System.out.println("home");
+        System.out.println("home page hamdler");
         model.addAttribute("name" ,"Substring Tech ");
         model.addAttribute("age" ,18);
         model.addAttribute("GithubRepo","https://github.com/monudhakad1/contactverse");
